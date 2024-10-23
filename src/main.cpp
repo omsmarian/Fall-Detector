6a8fd9d8a1e9178e32183ab33103f01aff53f2d3
Mariano <omsmarian@gmail.com>
2024-10-22 21:41:54 -0300

    mpu calibration and multi files

--- src/main.cpp
+++ src/main.cpp
@@ -1,384 +1,180 @@
+/**
+ * @file main.cpp
+ * @author Mariano Oms
+ * @brief Este archivo contiene el código principal del proyecto
+ * @date 2024-10-22
+ */
+
+/************************************************************************************
+ *                               Includes                                           *
+ ************************************************************************************/
 #include <Arduino.h>
-#include <ESP8266WiFi.h>
-/*
-#include <MPU6050.h>
-#include <Wire.h>
-#include <I2Cdev.h>
-
-
-MPU6050 sensor;
-int16_t ax, ay, az;
-int16_t gx, gy, gz;
-
-// Código de prueba para el sensor MPU6050  
-
-void setup() 
-{
-  Serial.begin(115200);
-  Wire.begin();
-  sensor.initialize();
-  Serial.println(sensor.testConnection() ? "MPU6050 connection successful" : "MPU6050 connection failed");
-}
-
-
-void loop() 
-{
-  // Leer las aceleraciones y velocidades angulares
-    sensor.getAcceleration(&ax, &ay, &az);
-    sensor.getRotation(&gx, &gy, &gz);
-
-    //Mostrar las lecturas separadas por un [tab]
-    Serial.print("a[x y z] g[x y z]:\t");
-    Serial.print(ax); Serial.print("\t");
-    Serial.print(ay); Serial.print("\t");
-    Serial.print(az); Serial.print("\t");
-    Serial.print(gx); Serial.print("\t");
-    Serial.print(gy); Serial.print("\t");
-    Serial.println(gz);
-
-    delay(100);
-}
-*/
-
-#include "I2Cdev.h"
 #include "MPU6050.h"
 
-// Arduino Wire library is required if I2Cdev I2CDEV_ARDUINO_WIRE implementation
-// is used in I2Cdev.h
-#if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
-    #include "Wire.h"
-#endif
 
-// class default I2C address is 0x68
-// specific I2C addresses may be passed as a parameter here
-// AD0 low = 0x68 (default for InvenSense evaluation board)
-// AD0 high = 0x69
-MPU6050 accelgyro;
-//MPU6050 accelgyro(0x69); // <-- use for AD0 high
+/************************************************************************************
+ *                              Macros and Defines                                  *
+ ************************************************************************************/
 
+#define INTERRUPT_PIN D7
+#define THRESHOLD 20
+#define DURATION 100
 
-const char LBRACKET = '[';
-const char RBRACKET = ']';
-const char COMMA    = ',';
-const char BLANK    = ' ';
-const char PERIOD   = '.';
+/************************************************************************************
+ *                                  Prototypes                                      *
+ ************************************************************************************/
 
-const int iAx = 0;
-const int iAy = 1;
-const int iAz = 2;
-const int iGx = 3;
-const int iGy = 4;
-const int iGz = 5;
+void SensorCalibration();
+void CheckFall();
 
-const int usDelay = 3150;   // empirical, to hold sampling to 200 Hz
-const int NFast =  1000;    // the bigger, the better (but slower)
-const int NSlow = 10000;    // ..
-const int LinesBetweenHeaders = 5;
-      int LowValue[6];
-      int HighValue[6];
-      int Smoothed[6];
-      int LowOffset[6];
-      int HighOffset[6];
-      int Target[6];
-      int LinesOut;
-      int N;
-      
-void SetAveraging(int NewN);
+/************************************************************************************
+ *                               Global Variables                                   *
+ ************************************************************************************/
 
-void ForceHeader()
-  { LinesOut = 99; }
-    
-void GetSmoothed()
-  { int16_t RawValue[6];
-    int i;
-    long Sums[6];
-    for (i = iAx; i <= iGz; i++)
-      { Sums[i] = 0; }
-//    unsigned long Start = micros();
-
-    for (i = 1; i <= N; i++)
-      { // get sums
-        accelgyro.getMotion6(&RawValue[iAx], &RawValue[iAy], &RawValue[iAz], 
-                             &RawValue[iGx], &RawValue[iGy], &RawValue[iGz]);
-        if ((i % 500) == 0)
-          Serial.print(PERIOD);
-        delayMicroseconds(usDelay);
-        for (int j = iAx; j <= iGz; j++)
-          Sums[j] = Sums[j] + RawValue[j];
-      } // get sums
-//    unsigned long usForN = micros() - Start;
-//    Serial.print(" reading at ");
-//    Serial.print(1000000/((usForN+N/2)/N));
-//    Serial.println(" Hz");
-    for (i = iAx; i <= iGz; i++)
-      { Smoothed[i] = (Sums[i] + N/2) / N ; }
-  } // GetSmoothed
-
-void Initialize()
-  {
-    // join I2C bus (I2Cdev library doesn't do this automatically)
-    #if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
-        Wire.begin();
-    #elif I2CDEV_IMPLEMENTATION == I2CDEV_BUILTIN_FASTWIRE
-        Fastwire::setup(400, true);
-    #endif
-
-    Serial.begin(9600);
-
-    // initialize device
-    Serial.println("Initializing I2C devices...");
-    accelgyro.initialize();
-
-    // verify connection
-    Serial.println("Testing device connections...");
-    Serial.println(accelgyro.testConnection() ? "MPU6050 connection successful" : "MPU6050 connection failed");
-    Serial.println("PID tuning Each Dot = 100 readings");
-  /*A tidbit on how PID (PI actually) tuning works. 
-    When we change the offset in the MPU6050 we can get instant results. This allows us to use Proportional and 
-    integral of the PID to discover the ideal offsets. Integral is the key to discovering these offsets, Integral 
-    uses the error from set-point (set-point is zero), it takes a fraction of this error (error * ki) and adds it 
-    to the integral value. Each reading narrows the error down to the desired offset. The greater the error from 
-    set-point, the more we adjust the integral value. The proportional does its part by hiding the noise from the 
-    integral math. The Derivative is not used because of the noise and because the sensor is stationary. With the 
-    noise removed the integral value lands on a solid offset after just 600 readings. At the end of each set of 100 
-    readings, the integral value is used for the actual offsets and the last proportional reading is ignored due to 
-    the fact it reacts to any noise.
-  */
-        accelgyro.CalibrateAccel(6);
-        accelgyro.CalibrateGyro(6);
-        Serial.println("\nat 600 Readings");
-        accelgyro.PrintActiveOffsets();
-        Serial.println();
-        accelgyro.CalibrateAccel(1);
-        accelgyro.CalibrateGyro(1);
-        Serial.println("700 Total Readings");
-        accelgyro.PrintActiveOffsets();
-        Serial.println();
-        accelgyro.CalibrateAccel(1);
-        accelgyro.CalibrateGyro(1);
-        Serial.println("800 Total Readings");
-        accelgyro.PrintActiveOffsets();
-        Serial.println();
-        accelgyro.CalibrateAccel(1);
-        accelgyro.CalibrateGyro(1);
-        Serial.println("900 Total Readings");
-        accelgyro.PrintActiveOffsets();
-        Serial.println();    
-        accelgyro.CalibrateAccel(1);
-        accelgyro.CalibrateGyro(1);
-        Serial.println("1000 Total Readings");
-        accelgyro.PrintActiveOffsets();
-     Serial.println("\n\n Any of the above offsets will work nice \n\n Lets proof the PID tuning using another method:"); 
-  } // Initialize
-
-void SetOffsets(int TheOffsets[6])
-  { accelgyro.setXAccelOffset(TheOffsets [iAx]);
-    accelgyro.setYAccelOffset(TheOffsets [iAy]);
-    accelgyro.setZAccelOffset(TheOffsets [iAz]);
-    accelgyro.setXGyroOffset (TheOffsets [iGx]);
-    accelgyro.setYGyroOffset (TheOffsets [iGy]);
-    accelgyro.setZGyroOffset (TheOffsets [iGz]);
-  } // SetOffsets
-
-void ShowProgress()
-  { if (LinesOut >= LinesBetweenHeaders)
-      { // show header
-        Serial.println("\tXAccel\t\t\tYAccel\t\t\t\tZAccel\t\t\tXGyro\t\t\tYGyro\t\t\tZGyro");
-        LinesOut = 0;
-      } // show header
-    Serial.print(BLANK);
-    for (int i = iAx; i <= iGz; i++)
-      { Serial.print(LBRACKET);
-        Serial.print(LowOffset[i]),
-        Serial.print(COMMA);
-        Serial.print(HighOffset[i]);
-        Serial.print("] --> [");
-        Serial.print(LowValue[i]);
-        Serial.print(COMMA);
-        Serial.print(HighValue[i]);
-        if (i == iGz)
-          { Serial.println(RBRACKET); }
-        else
-          { Serial.print("]\t"); }
-      }
-    LinesOut++;
-  } // ShowProgress
-
-void PullBracketsIn()
-  { boolean AllBracketsNarrow;
-    boolean StillWorking;
-    int NewOffset[6];
-  
-    Serial.println("\nclosing in:");
-    AllBracketsNarrow = false;
-    ForceHeader();
-    StillWorking = true;
-    while (StillWorking) 
-      { StillWorking = false;
-        if (AllBracketsNarrow && (N == NFast))
-          { SetAveraging(NSlow); }
-        else
-          { AllBracketsNarrow = true; }// tentative
-        for (int i = iAx; i <= iGz; i++)
-          { if (HighOffset[i] <= (LowOffset[i]+1))
-              { NewOffset[i] = LowOffset[i]; }
-            else
-              { // binary search
-                StillWorking = true;
-                NewOffset[i] = (LowOffset[i] + HighOffset[i]) / 2;
-                if (HighOffset[i] > (LowOffset[i] + 10))
-                  { AllBracketsNarrow = false; }
-              } // binary search
-          }
-        SetOffsets(NewOffset);
-        GetSmoothed();
-        for (int i = iAx; i <= iGz; i++)
-          { // closing in
-            if (Smoothed[i] > Target[i])
-              { // use lower half
-                HighOffset[i] = NewOffset[i];
-                HighValue[i] = Smoothed[i];
-              } // use lower half
-            else
-              { // use upper half
-                LowOffset[i] = NewOffset[i];
-                LowValue[i] = Smoothed[i];
-              } // use upper half
-          } // closing in
-        ShowProgress();
-      } // still working
-   
-  } // PullBracketsIn
-
-void PullBracketsOut()
-  { boolean Done = false;
-    int NextLowOffset[6];
-    int NextHighOffset[6];
-
-    Serial.println("expanding:");
-    ForceHeader();
+int counter=0;
+bool calibration_flag = false;
+MPU6050 sensor(MPU6050_ADDRESS_AD0_LOW);
  
-    while (!Done)
-      { Done = true;
-        SetOffsets(LowOffset);
-        GetSmoothed();
-        for (int i = iAx; i <= iGz; i++)
-          { // got low values
-            LowValue[i] = Smoothed[i];
-            if (LowValue[i] >= Target[i])
-              { Done = false;
-                NextLowOffset[i] = LowOffset[i] - 1000;
-              }
-            else
-              { NextLowOffset[i] = LowOffset[i]; }
-          } // got low values
-      
-        SetOffsets(HighOffset);
-        GetSmoothed();
-        for (int i = iAx; i <= iGz; i++)
-          { // got high values
-            HighValue[i] = Smoothed[i];
-            if (HighValue[i] <= Target[i])
-              { Done = false;
-                NextHighOffset[i] = HighOffset[i] + 1000;
-              }
-            else
-              { NextHighOffset[i] = HighOffset[i]; }
-          } // got high values
-        ShowProgress();
-        for (int i = iAx; i <= iGz; i++)
-          { LowOffset[i] = NextLowOffset[i];   // had to wait until ShowProgress done
-            HighOffset[i] = NextHighOffset[i]; // ..
-          }
-     } // keep going
-  } // PullBracketsOut
 
-void SetAveraging(int NewN)
-  { N = NewN;
-    Serial.print("averaging ");
-    Serial.print(N);
-    Serial.println(" readings each time");
-   } // SetAveraging
+const uint8_t threshold = 20;
+const uint8_t duration = 10;
+ 
+
+/************************************************************************************
+ *                                  Main                                            *
+ ************************************************************************************/
 
 void setup()
-  { Initialize();
-    for (int i = iAx; i <= iGz; i++)
-      { // set targets and initial guesses
-        Target[i] = 0; // must fix for ZAccel 
-        HighOffset[i] = 0;
-        LowOffset[i] = 0;
-      } // set targets and initial guesses
-    Target[iAz] = 16384;
-    SetAveraging(NFast);
-    
-    PullBracketsOut();
-    PullBracketsIn();
-    
-    Serial.println("-------------- done --------------");
-  } // setup
+{
+Wire.begin();
+Serial.begin(115200);
+Serial.println("Hello User");
+
+// Calibration
+SensorCalibration();
+
+}
  
 void loop()
+{
+  CheckFall();
+}
+
+/************************************************************************************
+ *                               Local Functions                                    *
+ ************************************************************************************/
+
+void SensorCalibration() {
+
+// Valores RAW (sin procesar) del acelerometro y giroscopio en los ejes x,y,z
+int16_t ax = 0, ay = 0, az = 0;
+int16_t gx = 0, gy = 0, gz = 0;
+
+//Variables usadas por el filtro pasa bajos
+long f_ax = 0,f_ay = 0, f_az = 0;
+int p_ax = 0, p_ay = 0, p_az = 0;
+long f_gx = 0,f_gy = 0, f_gz = 0;
+int p_gx = 0, p_gy = 0, p_gz = 0;
+
+//Valor de los offsets
+int ax_o = 0, ay_o = 0, az_o = 0;
+int gx_o = 0, gy_o = 0, gz_o = 0;
+
+sensor.initialize();
+
+// Printeo
+Serial.println("Calibrando, no mover IMU");  
+
+while(!calibration_flag)
+{
+  // Leer las aceleraciones y velocidades angulares
+  sensor.getAcceleration(&ax, &ay, &az);
+  sensor.getRotation(&gx, &gy, &gz);
+
+  // Filtrar las lecturas
+  f_ax = f_ax-(f_ax>>5)+ax;
+  p_ax = f_ax>>5;
+
+  f_ay = f_ay-(f_ay>>5)+ay;
+  p_ay = f_ay>>5;
+
+  f_az = f_az-(f_az>>5)+az;
+  p_az = f_az>>5;
+
+  f_gx = f_gx-(f_gx>>3)+gx;
+  p_gx = f_gx>>3;
+
+  f_gy = f_gy-(f_gy>>3)+gy;
+  p_gy = f_gy>>3;
+
+  f_gz = f_gz-(f_gz>>3)+gz;
+  p_gz = f_gz>>3;
+
+  //Cada 100 lecturas corregir el offset
+  if (counter==100)
   {
-  } // loop
+    //Mostrar las lecturas separadas por un [tab]
+    Serial.print("promedio:"); Serial.print("t");
+    Serial.print(p_ax); Serial.print("\t");
+    Serial.print(p_ay); Serial.print("\t");
+    Serial.print(p_az); Serial.print("\t");
+    Serial.print(p_gx); Serial.print("\t");
+    Serial.print(p_gy); Serial.print("\t");
+    Serial.println(p_gz);
 
-/*
-#include <ESP8266WiFi.h>
-#include <ESP8266HTTPClient.h>
-#include <WiFiClient.h>
-#include <UrlEncode.h>
+    //Calibrar el acelerometro a 1g en el eje z (ajustar el offset)
+    if (p_ax>0) ax_o--;
+    else {ax_o++;}
+    if (p_ay>0) ay_o--;
+    else {ay_o++;}
+    if (p_az-16384>0) az_o--;
+    else {az_o++;}
+    
+    sensor.setXAccelOffset(ax_o);
+    sensor.setYAccelOffset(ay_o);
+    sensor.setZAccelOffset(az_o);
 
-const char* ssid = "Mariano";
-const char* password = "hola1234";
+    //Calibrar el giroscopio a 0º/s en todos los ejes (ajustar el offset)
+    if (p_gx>0) gx_o--;
+    else {gx_o++;}
+    if (p_gy>0) gy_o--;
+    else {gy_o++;}
+    if (p_gz>0) gz_o--;
+    else {gz_o++;}
+    
+    sensor.setXGyroOffset(gx_o);
+    sensor.setYGyroOffset(gy_o);
+    sensor.setZGyroOffset(gz_o);
 
-// +international_country_code + phone number
-// Portugal +351, example: +351912345678
-String phoneNumber = "5491169350147";
-String apiKey = "9384670";
-
-void sendMessage(String message){
-
-  // Data to send with HTTP POST
-  String url = "http://api.callmebot.com/whatsapp.php?phone=" + phoneNumber + "&apikey=" + apiKey + "&text=" + urlEncode(message);
-  WiFiClient client;    
-  HTTPClient http;
-  http.begin(client, url);
-
-  // Specify content-type header
-  http.addHeader("Content-Type", "application/x-www-form-urlencoded");
-  
-  // Send HTTP POST request
-  int httpResponseCode = http.POST(url);
-  if (httpResponseCode == 200){
-    Serial.print("Message sent successfully");
+    // Get out of loop
+    calibration_flag = true;
   }
-  else{
-    Serial.println("Error sending the message");
-    Serial.print("HTTP response code: ");
-    Serial.println(httpResponseCode);
+  counter++;
   }
-
-  // Free resources
-  http.end();
 }
 
-void setup() {
-  Serial.begin(115200);
+void CheckFall()
+{
+  int16_t ax = 0, ay = 0, az = 0;
+  int16_t gx = 0, gy = 0, gz = 0;
 
-  WiFi.begin(ssid, password);
-  Serial.println("Connecting");
-  while(WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-  Serial.println("");
-  Serial.print("Connected to WiFi network with IP Address: ");
-  Serial.println(WiFi.localIP());
+   // Leer las aceleraciones y velocidades angulares
+  sensor.getAcceleration(&ax, &ay, &az);
+  sensor.getRotation(&gx, &gy, &gz);
+  float ax_m_s2 = ax * (9.81/16384.0);
+  float ay_m_s2 = ay * (9.81/16384.0);
+  float az_m_s2 = az * (9.81/16384.0);
+  float gx_deg_s = gx * (250.0/32768.0);
+  float gy_deg_s = gy * (250.0/32768.0);
+  float gz_deg_s = gz * (250.0/32768.0);
+  //Mostrar las lecturas separadas por un [tab]
+  Serial.print("a[x y z](m/s2) g[x y z](deg/s):\t");
+  Serial.print(ax_m_s2); Serial.print("\t");
+  Serial.print(ay_m_s2); Serial.print("\t");
+  Serial.print(az_m_s2); Serial.print("\t");
+  Serial.print(gx_deg_s); Serial.print("\t");
+  Serial.print(gy_deg_s); Serial.print("\t");
+  Serial.println(gz_deg_s);
 
-  // Send Message to WhatsAPP
-  sendMessage("Hello from ESP8266!");
+  delay(100);
 }
-
-void loop() {
-
-}
-*/
