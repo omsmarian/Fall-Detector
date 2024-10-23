4a01a3a186ead8478153e2f253c30922fe9c5d1d
omsmarian <omsmarian@gmail.com>
2024-10-23 03:32:43 -0300

    no anda nada

--- src/Wifi.cpp
+++ src/Wifi.cpp
@@ -1,3 +1,6 @@
+#include <Arduino.h>
+#include <ArduinoJson.h>
+#include <MPU6050.h>
 #include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h>
 #include <WiFiClient.h>
@@ -5,6 +8,21 @@
 
 const char* ssid = "Mariano";
 const char* password = "hola1234";
+
+// unwiredlabs Hostname & Geolocation Endpoint url
+const char* Host = "www.unwiredlabs.com";
+String endpoint = "/v2/process.php";
+
+// UnwiredLabs API_Token. Signup here to get a free token https://unwiredlabs.com/trial
+String token = "pk.e3cd9d7c4f54963964e927d90a2ae160";
+
+String jsonString = "{\n";
+
+// Variables to store unwiredlabs response
+double latitude = 0.0;
+double longitude = 0.0;
+double accuracy = 0.0;
+
 
 // +international_country_code + phone number
 // Portugal +351, example: +351912345678
@@ -37,9 +55,16 @@ void sendMessage(String message){
   http.end();
 }
 
+
+
+
 void setup() {
   Serial.begin(115200);
 
+  WiFi.mode(WIFI_STA);
+  WiFi.disconnect();
+  Serial.println("Setup done");
+  
   WiFi.begin(ssid, password);
   Serial.println("Connecting");
   while(WiFi.status() != WL_CONNECTED) {
@@ -47,13 +72,100 @@ void setup() {
     Serial.print(".");
   }
   Serial.println("");
-  Serial.print("Connected to WiFi network with IP Address: ");
-  Serial.println(WiFi.localIP());
-
-  // Send Message to WhatsAPP
-  sendMessage("Hello from ESP8266!");
+  Serial.print("Connected");
 }
+
 
 void loop() {
 
+  
+
+  // WiFi.scanNetworks will return the number of networks found
+  int n = WiFi.scanNetworks();
+  Serial.println("scan done");
+
+  if (n == 0 ) 
+  {
+    Serial.println("No networks available");
+  }
+  else
+  {
+    Serial.print(n);
+    Serial.println(" networks found");
+  }
+
+  // now build the jsonString...
+  jsonString = "{\n";
+  jsonString += "\"token\" : \"";
+  jsonString += token;
+  jsonString += "\",\n";
+  jsonString += "\"id\" : \"saikirandevice01\",\n";
+  jsonString += "\"wifi\": [\n";
+  for (int j = 0; j < n; ++j) 
+  {
+    jsonString += "{\n";
+    jsonString += "\"bssid\" : \"";
+    jsonString += (WiFi.BSSIDstr(j));
+    jsonString += "\",\n";
+    jsonString += "\"signal\": ";
+    jsonString += WiFi.RSSI(j);
+    jsonString += "\n";
+    if (j < n - 1)
+    {
+      jsonString += "},\n";
+    }
+    else
+    {
+      jsonString += "}\n";
+    }
+  }
+  jsonString += ("]\n");
+  jsonString += ("}\n");
+  Serial.println(jsonString);
+
+  WiFiClientSecure client;
+
+    //Connect to the client and make the api call
+    Serial.println("Requesting URL: https://" + (String)Host + endpoint);
+    if (client.connect(Host, 443))
+    {
+      Serial.println("Connected");
+      client.println("POST " + endpoint + " HTTP/1.1");
+      client.println("Host: " + (String)Host);
+      client.println("Connection: close");
+      client.println("Content-Type: application/json");
+      client.println("User-Agent: Arduino/1.0");
+      client.print("Content-Length: ");
+      client.println(jsonString.length());
+      client.println();
+      client.print(jsonString);
+      delay(500);
+    }
+
+    //Read and parse all the lines of the reply from server
+    while (client.available())
+    {
+      String line = client.readStringUntil('\r');
+      JsonObject& root = jsonBuffer.parseObject(line);
+      if (root.success()) 
+      {
+        latitude    = root["lat"];
+        longitude   = root["lon"];
+        accuracy    = root["accuracy"];
+
+        Serial.println();
+        Serial.print("Latitude = ");
+        Serial.println(latitude, 6);
+        Serial.print("Longitude = ");
+        Serial.println(longitude, 6);
+        Serial.print("Accuracy = ");
+        Serial.println(accuracy);
+      }
+    }
+
+    Serial.println("closing connection");
+    Serial.println();
+    client.stop();
+
+      delay(5000);
 }
--- src/main.cpp
+++ src/main.cpp
@@ -16,28 +16,24 @@
  *                              Macros and Defines                                  *
  ************************************************************************************/
 
-#define INTERRUPT_PIN D7
-#define THRESHOLD 20
-#define DURATION 100
+#define DELAY 20
 
 /************************************************************************************
  *                                  Prototypes                                      *
  ************************************************************************************/
 
-void SensorCalibration();
-void CheckFall();
 
 /************************************************************************************
  *                               Global Variables                                   *
  ************************************************************************************/
 
-int counter=0;
-bool calibration_flag = false;
 MPU6050 sensor(MPU6050_ADDRESS_AD0_LOW);
  
+uint32_t now;
+uint32_t then;
+int16_t ax, ay, az;
+float x, y, z;
 
-const uint8_t threshold = 20;
-const uint8_t duration = 10;
  
 
 /************************************************************************************
@@ -49,132 +45,40 @@ void setup()
 Wire.begin();
 Serial.begin(115200);
 Serial.println("Hello User");
+sensor.initialize();
 
-// Calibration
-SensorCalibration();
+if (sensor.testConnection()) 
+  Serial.println("Sensor iniciado correctamente");
+else 
+  Serial.println("Error al iniciar el sensor");
 
 }
  
 void loop()
 {
-  CheckFall();
+  now = millis();
+  if (now - then >= DELAY)
+  {
+    // update time
+    then = now;
+
+    // get RAW data
+    sensor.getAcceleration(&ax, &ay, &az);
+
+    // convert to g
+    x = ax / 16384.0;
+    y = ay / 16384.0;
+    z = az / 16384.0;
+
+    // print data
+    Serial.print(x, 5);
+    Serial.print(",");
+    Serial.print(y, 5);
+    Serial.print(",");
+    Serial.println(z, 5);
+  }
 }
 
 /************************************************************************************
  *                               Local Functions                                    *
  ************************************************************************************/
-
-void SensorCalibration() {
-
-// Valores RAW (sin procesar) del acelerometro y giroscopio en los ejes x,y,z
-int16_t ax = 0, ay = 0, az = 0;
-int16_t gx = 0, gy = 0, gz = 0;
-
-//Variables usadas por el filtro pasa bajos
-long f_ax = 0,f_ay = 0, f_az = 0;
-int p_ax = 0, p_ay = 0, p_az = 0;
-long f_gx = 0,f_gy = 0, f_gz = 0;
-int p_gx = 0, p_gy = 0, p_gz = 0;
-
-//Valor de los offsets
-int ax_o = 0, ay_o = 0, az_o = 0;
-int gx_o = 0, gy_o = 0, gz_o = 0;
-
-sensor.initialize();
-
-// Printeo
-Serial.println("Calibrando, no mover IMU");  
-
-while(!calibration_flag)
-{
-  // Leer las aceleraciones y velocidades angulares
-  sensor.getAcceleration(&ax, &ay, &az);
-  sensor.getRotation(&gx, &gy, &gz);
-
-  // Filtrar las lecturas
-  f_ax = f_ax-(f_ax>>5)+ax;
-  p_ax = f_ax>>5;
-
-  f_ay = f_ay-(f_ay>>5)+ay;
-  p_ay = f_ay>>5;
-
-  f_az = f_az-(f_az>>5)+az;
-  p_az = f_az>>5;
-
-  f_gx = f_gx-(f_gx>>3)+gx;
-  p_gx = f_gx>>3;
-
-  f_gy = f_gy-(f_gy>>3)+gy;
-  p_gy = f_gy>>3;
-
-  f_gz = f_gz-(f_gz>>3)+gz;
-  p_gz = f_gz>>3;
-
-  //Cada 100 lecturas corregir el offset
-  if (counter==100)
-  {
-    //Mostrar las lecturas separadas por un [tab]
-    Serial.print("promedio:"); Serial.print("t");
-    Serial.print(p_ax); Serial.print("\t");
-    Serial.print(p_ay); Serial.print("\t");
-    Serial.print(p_az); Serial.print("\t");
-    Serial.print(p_gx); Serial.print("\t");
-    Serial.print(p_gy); Serial.print("\t");
-    Serial.println(p_gz);
-
-    //Calibrar el acelerometro a 1g en el eje z (ajustar el offset)
-    if (p_ax>0) ax_o--;
-    else {ax_o++;}
-    if (p_ay>0) ay_o--;
-    else {ay_o++;}
-    if (p_az-16384>0) az_o--;
-    else {az_o++;}
-    
-    sensor.setXAccelOffset(ax_o);
-    sensor.setYAccelOffset(ay_o);
-    sensor.setZAccelOffset(az_o);
-
-    //Calibrar el giroscopio a 0º/s en todos los ejes (ajustar el offset)
-    if (p_gx>0) gx_o--;
-    else {gx_o++;}
-    if (p_gy>0) gy_o--;
-    else {gy_o++;}
-    if (p_gz>0) gz_o--;
-    else {gz_o++;}
-    
-    sensor.setXGyroOffset(gx_o);
-    sensor.setYGyroOffset(gy_o);
-    sensor.setZGyroOffset(gz_o);
-
-    // Get out of loop
-    calibration_flag = true;
-  }
-  counter++;
-  }
-}
-
-void CheckFall()
-{
-  int16_t ax = 0, ay = 0, az = 0;
-  int16_t gx = 0, gy = 0, gz = 0;
-
-   // Leer las aceleraciones y velocidades angulares
-  sensor.getAcceleration(&ax, &ay, &az);
-  sensor.getRotation(&gx, &gy, &gz);
-  float ax_m_s2 = ax * (9.81/16384.0);
-  float ay_m_s2 = ay * (9.81/16384.0);
-  float az_m_s2 = az * (9.81/16384.0);
-  float gx_deg_s = gx * (250.0/32768.0);
-  float gy_deg_s = gy * (250.0/32768.0);
-  float gz_deg_s = gz * (250.0/32768.0);
-  //Mostrar las lecturas separadas por un [tab]
-  Serial.print("a[x y z](m/s2) g[x y z](deg/s):\t");
-  Serial.print(ax_m_s2); Serial.print("\t");
-  Serial.print(ay_m_s2); Serial.print("\t");
-  Serial.print(az_m_s2); Serial.print("\t");
-  Serial.print(gx_deg_s); Serial.print("\t");
-  Serial.print(gy_deg_s); Serial.print("\t");
-  Serial.println(gz_deg_s);
-
-  delay(100);
-}
