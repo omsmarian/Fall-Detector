d9599888dbd6ea40905b6760b01231e354008f7f
Mariano <omsmarian@gmail.com>
2024-10-24 02:02:46 -0300

    anda v1

--- src/Wifi.cpp
+++ src/Wifi.cpp
@@ -1,27 +1,25 @@
 #include <Arduino.h>
-#include <ArduinoJson.h>
+#include <Wire.h>
+#include <I2Cdev.h>
 #include <MPU6050.h>
 #include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h>
 #include <WiFiClient.h>
 #include <UrlEncode.h>
 
-const char* ssid = "Mariano";
-const char* password = "hola1234";
 
-// unwiredlabs Hostname & Geolocation Endpoint url
-const char* Host = "www.unwiredlabs.com";
-String endpoint = "/v2/process.php";
+#define FALL_THRESHOLD 0.7
 
-// UnwiredLabs API_Token. Signup here to get a free token https://unwiredlabs.com/trial
-String token = "pk.e3cd9d7c4f54963964e927d90a2ae160";
+const char* ssid = "TCL 408";
+const char* password = "apik6Vis";
 
-String jsonString = "{\n";
+bool flag;
 
-// Variables to store unwiredlabs response
-double latitude = 0.0;
-double longitude = 0.0;
-double accuracy = 0.0;
+int16_t ax = 0, ay = 0, az = 0;
+float ax_g, ay_g, az_g;
+float prevax_g, prevay_g, prevaz_g;
+
+MPU6050 accelgyro(0x68); // <-- use for AD0 high
 
 
 // +international_country_code + phone number
@@ -29,8 +27,115 @@ double accuracy = 0.0;
 String phoneNumber = "5491169350147";
 String apiKey = "9384670";
 
-void sendMessage(String message){
+void getAccelerationG();
+bool fallCheck();
+void sendMessage(String message);
 
+void setup() {
+  pinMode(LED_BUILTIN,OUTPUT);
+  Serial.begin(115200);
+
+  WiFi.mode(WIFI_STA);
+  WiFi.disconnect();
+  Wire.begin();
+  accelgyro.initialize();
+
+  if (accelgyro.testConnection()) Serial.println("Sensor iniciado correctamente");
+  else Serial.println("Error al iniciar el sensor");
+  
+  delay(1000);
+
+  Serial.println("Setup done");
+  
+  WiFi.begin(ssid, password);
+  Serial.println("Connecting");
+  while(WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("Connected");
+
+  accelgyro.setXAccelOffset(-4301);
+  accelgyro.setYAccelOffset(-448);
+  accelgyro.setZAccelOffset(2375);
+  
+  getAccelerationG();
+  prevax_g = ax_g;
+  prevay_g = ay_g;
+  prevaz_g = az_g;
+}
+
+
+void loop() 
+{
+  digitalWrite(LED_BUILTIN, LOW);
+  getAccelerationG();
+  
+  Serial.println("Actual:");
+
+  Serial.print(ax_g);
+  Serial.print(",");
+  Serial.print(ay_g);
+  Serial.print(",");
+  Serial.println(az_g);
+
+  Serial.println("Prev:");
+
+  Serial.print(prevax_g);
+  Serial.print(",");
+  Serial.print(prevay_g);
+  Serial.print(",");
+  Serial.println(prevaz_g);
+
+  if(fallCheck())
+  {
+    flag = true;
+  }
+
+  if(flag)
+  {
+    digitalWrite(LED_BUILTIN, HIGH);
+    Serial.println("Detectado");
+    String mensaje = "Ayuda";
+    sendMessage(mensaje);
+    flag = false;
+    delay(3000);
+  }
+
+  delay(1000);
+}
+
+
+void getAccelerationG()
+{
+  accelgyro.getAcceleration(&ax,&ay,&az);
+  ax_g = ax / 16384.0;
+  ay_g = ay / 16384.0;
+  az_g = az / 16384.0;
+}
+
+bool fallCheck()
+{
+  bool fall = false;
+  if(abs(ax_g - prevax_g) > FALL_THRESHOLD)
+    fall = true;
+  else if (abs(ay_g - prevay_g) > FALL_THRESHOLD)
+    fall = true;
+  else if (abs(az_g - prevaz_g) > FALL_THRESHOLD)
+    fall = true;
+  else
+    fall = false;
+
+  prevax_g = ax_g;
+  prevay_g = ay_g;
+  prevaz_g = az_g;
+
+  return fall;
+}
+
+void sendMessage(String message)
+{
   // Data to send with HTTP POST
   String url = "http://api.callmebot.com/whatsapp.php?phone=" + phoneNumber + "&apikey=" + apiKey + "&text=" + urlEncode(message);
   WiFiClient client;    
@@ -53,119 +158,4 @@ void sendMessage(String message){
 
   // Free resources
   http.end();
-}
-
-
-
-
-void setup() {
-  Serial.begin(115200);
-
-  WiFi.mode(WIFI_STA);
-  WiFi.disconnect();
-  Serial.println("Setup done");
-  
-  WiFi.begin(ssid, password);
-  Serial.println("Connecting");
-  while(WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-  Serial.println("");
-  Serial.print("Connected");
-}
-
-
-void loop() {
-
-  DynamicJsonBuffer jsonBuffer;
-
-  // WiFi.scanNetworks will return the number of networks found
-  int n = WiFi.scanNetworks();
-  Serial.println("scan done");
-
-  if (n == 0 ) 
-  {
-    Serial.println("No networks available");
-  }
-  else
-  {
-    Serial.print(n);
-    Serial.println(" networks found");
-  }
-
-  // now build the jsonString...
-  jsonString = "{\n";
-  jsonString += "\"token\" : \"";
-  jsonString += token;
-  jsonString += "\",\n";
-  jsonString += "\"id\" : \"saikirandevice01\",\n";
-  jsonString += "\"wifi\": [\n";
-  for (int j = 0; j < n; ++j) 
-  {
-    jsonString += "{\n";
-    jsonString += "\"bssid\" : \"";
-    jsonString += (WiFi.BSSIDstr(j));
-    jsonString += "\",\n";
-    jsonString += "\"signal\": ";
-    jsonString += WiFi.RSSI(j);
-    jsonString += "\n";
-    if (j < n - 1)
-    {
-      jsonString += "},\n";
-    }
-    else
-    {
-      jsonString += "}\n";
-    }
-  }
-  jsonString += ("]\n");
-  jsonString += ("}\n");
-  Serial.println(jsonString);
-
-  WiFiClientSecure client;
-
-    //Connect to the client and make the api call
-    Serial.println("Requesting URL: https://" + (String)Host + endpoint);
-    if (client.connect(Host, 443))
-    {
-      Serial.println("Connected");
-      client.println("POST " + endpoint + " HTTP/1.1");
-      client.println("Host: " + (String)Host);
-      client.println("Connection: close");
-      client.println("Content-Type: application/json");
-      client.println("User-Agent: Arduino/1.0");
-      client.print("Content-Length: ");
-      client.println(jsonString.length());
-      client.println();
-      client.print(jsonString);
-      delay(500);
-    }
-
-    //Read and parse all the lines of the reply from server
-    while (client.available())
-    {
-      String line = client.readStringUntil('\r');
-      JsonObject& root = jsonBuffer.parseObject(line);
-      if (root.success()) 
-      {
-        latitude    = root["lat"];
-        longitude   = root["lon"];
-        accuracy    = root["accuracy"];
-
-        Serial.println();
-        Serial.print("Latitude = ");
-        Serial.println(latitude, 6);
-        Serial.print("Longitude = ");
-        Serial.println(longitude, 6);
-        Serial.print("Accuracy = ");
-        Serial.println(accuracy);
-      }
-    }
-
-    Serial.println("closing connection");
-    Serial.println();
-    client.stop();
-
-      delay(5000);
 }
